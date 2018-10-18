5faf900ef008e21b1d4becbb36d1f66065d8328d
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-10-18 10:37:31 +0200

    implement normal equation

--- assignment1/ex1/ex1_multi.m
+++ assignment1/ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.3;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -154,7 +154,8 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+new_entry2 = [1, 1650,3];
+price = (new_entry2)*(theta);
 
 % ============================================================
 
