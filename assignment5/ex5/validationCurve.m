274c85e8f876e0de93c206ed6cd7dd55de4cdc3b
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-10-24 15:09:17 +0200

    implement validationCurve

--- assignment5/ex5/validationCurve.m
+++ assignment5/ex5/validationCurve.m
@@ -41,7 +41,12 @@ error_val = zeros(length(lambda_vec), 1);
 
 
 
-
+ for i = 1:length(lambda_vec)
+  lambda = lambda_vec(i);
+  theta2 = trainLinearReg(X(1:i, :), y(1:i), lambda);
+  error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i), theta2, lambda);
+  error_val(i) = linearRegCostFunction(Xval, yval, theta2, lambda);
+end
 
 
 
