cf463bc2717c7679b51d5ea08c54341a21bbc4aa
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-11-03 14:26:14 +0100

    implement the automatic choosen of C and sigma

--- assignment6/ex6/dataset3Params.m
+++ assignment6/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,12 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+all_predictions = zeros(size(values, 2).*size(values, 2), 3);
+row_store = 1;
 
 
+for i = 1:size(values, 2),
+	for j = 1:size(values, 2),
+		C = values(i);
+		sigma = values(j);
+		model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 		
+		predictions = svmPredict(model, Xval);
+		accuracy = mean(double(predictions ~= yval));
+		all_predictions(row_store, 1) = C;
+		all_predictions(row_store, 2) = sigma;
+		all_predictions(row_store, 3) = accuracy;
+		row_store += 1;
+	end
+end
 
-
-
-
+[max_values indices] = min(all_predictions);
+max_accuracy_index = indices(1, 3);
+C = all_predictions(max_accuracy_index, 1)
+sigma = all_predictions(max_accuracy_index, 2)
+all_predictions
 % =========================================================================
 
 end
