57a8ac328097dfea0713c066b32ec4447fd22249
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-11-22 20:14:27 +0100

    Implement kMeansInitCentroids

--- assignment7/ex7/computeCentroids.m
+++ assignment7/ex7/computeCentroids.m
@@ -29,24 +29,14 @@ centroids = zeros(K, n);
 new_centroids = zeros(K, n + 1);
 
 for i=1:m
-<<<<<<< HEAD
 	new_centroids(idx(i), 1:n) += X(i, :);
 	new_centroids(idx(i), n + 1) += 1;
-=======
-	new_centroids(idx(i), 1:2) += X(i, :);
-	new_centroids(idx(i), 3) += 1;
->>>>>>> c0bdfeecc4f0c362ad2f0d95dcb59b822ed2eb59
 end
 
 
 for i=1:K
-<<<<<<< HEAD
 	div = new_centroids(i, n + 1);
 	centroids(i, :) = new_centroids(i, 1:n)./div;
-=======
-	div = new_centroids(i, 3);
-	centroids(i, :) = new_centroids(i, 1:2)./div;
->>>>>>> c0bdfeecc4f0c362ad2f0d95dcb59b822ed2eb59
 end
 
 
