8b8190e68f367a6df530c6c6960c0f01a64729d0
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-11-29 16:46:48 +0100

    fix estimateGaussian

--- assignment8/ex8/estimateGaussian.m
+++ assignment8/ex8/estimateGaussian.m
@@ -21,15 +21,9 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-mu = (1./m)*sum(X);
 
-sigma2 = (1./m)*(sum(X.-mu).^2);
-sigma2 = sigma2';
-
-
-
-
-
+mu = ((1./m)*sum(X))';
+sigma2 = (std(X, 1).^2)';
 
 
 
